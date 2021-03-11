39a33ed6bd9ec7d17a2e162c0a276d1baf43a50f
noampolak <noampolak@gmail.com>
2021-03-11 11:46:22 +0200

    fix wsgi app name - adl_lrs

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ pip install -r /code/ADL_LRS/requirements.txt
 
 # service nginx restart
 
-cd /code/ADL_LRS && gunicorn --bind 0.0.0.0:8080  --workers=9 skillz.wsgi:application --log-level=debug -c /code/ADL_LRS/docker-gunicorn.conf.py
+cd /code/ADL_LRS && gunicorn --bind 0.0.0.0:8080  --workers=9 adl_lrs.wsgi:application --log-level=debug -c /code/ADL_LRS/docker-gunicorn.conf.py
 
