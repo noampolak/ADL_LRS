f9fe38544440d087377db29b002d4a2931744b91
noampolak <noampolak@gmail.com>
2021-03-11 13:56:43 +0200

    add options to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,25 @@ pip install -r /code/ADL_LRS/requirements.txt
 
 # service nginx restart
 
-cd /code/ADL_LRS && gunicorn --bind 0.0.0.0:8080  --workers=9 adl_lrs.wsgi:application --log-level=debug -c /code/ADL_LRS/docker-gunicorn.conf.py
-
+# cd /code/ADL_LRS && gunicorn --bind 0.0.0.0:8080  --workers=9 adl_lrs.wsgi:application --log-level=debug -c /code/ADL_LRS/docker-gunicorn.conf.py
+case $1 in
+pause)
+    sleep 24h
+    ;;
+statics)
+    shift
+    echo "Rsync statics From:  /code/skillzdjango/skillz/statics To: /statics.."
+    rsync -avz --update --delete /code/skillzdjango/skillz/statics /
+   ;;
+manage)
+    shift
+    cd /code/ADL_LRS/adl_lrs && python manage.py "$@"
+    ;;
+gunicorn)
+	 cd /code/ADL_LRS && gunicorn --bind 0.0.0.0:8080  --workers=9 adl_lrs.wsgi:application --log-level=debug -c /code/ADL_LRS/docker-gunicorn.conf.py
+	;;
+*)
+    echo "Running exec $@ .."
+    exit $?
+    ;;
+esac
